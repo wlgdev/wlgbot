651bdc906ac66a0a8f9ee7c6a91426b06cef12f1
Yuriy <yuramazh@gmail.com>
2024-02-29 21:13:04 +0100

    initial code created

---
+++ src/main/java/tv/wlg/bot/TwitchBotApplication.java
@@ -0,0 +1,10 @@
+package tv.wlg.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchBotApplication {
+    public static void main(String[] args) {
+    }
+}
